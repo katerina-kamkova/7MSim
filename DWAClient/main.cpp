aaaadd80b3dc42c7c66ab26098c19e3271d6eab4
Yaroslav Koekin <ykoekin@gmail.com>
2020-10-13 12:07:42 +0300

    added tcp server

--- DWAClient/main.cpp
+++ DWAClient/main.cpp
@@ -1,24 +1,66 @@
 #include "ObstacleAvoidance.h"
 #include "world_data.pb.h"
-#include "SafeQueue.h"
+#include "TCPServer.h"
 
 using namespace std;
 
-SafeQueue<protocol::WorldData> safeWorldDataQueue;
+const int port = 4343;
+Client supervisorClient, robotClient;
 
-void sendData(const protocol::WheelsVelocity& velocity);
+void sendWorldDataToClient(const Client& client, const protocol::WheelsVelocity& velocity);
+void DistributeNewClient(Client newClient);
+protocol::WorldData ReceiveWorldDataFromClient(const Client& client);
 
-void sendData(const protocol::WheelsVelocity& velocity) {
+void sendWorldDataToClient(const Client& client, const protocol::WheelsVelocity& velocity) {
+    int size = velocity.ByteSize();
+    char* velocityArray = new char[size];
+    velocity.SerializeToArray(velocityArray, size);
+
+    TCPServer::SendToClient(client, velocityArray, size);
+}
+
+void DistributeNewClient(Client newClient)
+{
+    if (newClient.clientType == supervisor)
+    {
+        supervisorClient = newClient;
+    }
+    else
+    {
+        robotClient = newClient;
+    }
 
 }
 
+protocol::WorldData ReceiveWorldDataFromClient(const Client& client)
+{
+    char worldDataArray[1024] = {0};
+    int size = TCPServer::ReceiveFromClient(client, worldDataArray);
+
+    protocol::WorldData newWorldData;
+
+    if (!newWorldData.ParseFromArray(worldDataArray, size))
+    {
+        perror("Failed to parse worldData from array");
+        exit(EXIT_FAILURE);
+    }
+
+    return newWorldData;
+}
+
 int main() {
+    TCPServer tcpServer(port);
+    tcpServer.StartListening();
 
-    protocol::WorldData worldData;
-    worldData.CopyFrom(safeWorldDataQueue.dequeue());
+    DistributeNewClient(tcpServer.AcceptNewClient());
+    DistributeNewClient(tcpServer.AcceptNewClient());
 
-    protocol::WheelsVelocity velocityData;
-    velocityData.CopyFrom(ObstacleAvoidance::dwa(worldData));
+    while (true) {
+        protocol::WorldData worldData = ReceiveWorldDataFromClient(supervisorClient);
 
-    sendData(velocityData);
+        protocol::WheelsVelocity velocityData;
+        velocityData.CopyFrom(ObstacleAvoidance::dwa(worldData));
+
+        sendWorldDataToClient(robotClient, velocityData);
+    }
 }

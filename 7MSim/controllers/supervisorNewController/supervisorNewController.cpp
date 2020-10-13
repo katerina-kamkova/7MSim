0486896d8c235c82a0c4634d65af9cdec1f6b4d1
katerina <katerina.kamkova@mail.ru>
2020-10-13 15:17:13 +0300

    fix problem

--- 7MSim/controllers/supervisorNewController/supervisorNewController.cpp
+++ 7MSim/controllers/supervisorNewController/supervisorNewController.cpp
@@ -1,42 +1,76 @@
-#include <webots/Robot.hpp>
 #include <webots/Supervisor.hpp>
+#include <webots/Robot.hpp>
+#include <webots/Motor.hpp>
+
+#include "client.cpp"
 
 using namespace webots;
 
 int main(int argc, char **argv) {
-  Robot *robot = new Robot();
-  int timeStep = (int)robot->getBasicTimeStep();
-
-  const double firstLineStart = -2.75; 
-  const double boundaries = 2;
-  const int obstaclesNUmber = 7;
+  Supervisor *supervisor = new Supervisor();
+  int timeStep = (int)supervisor->getBasicTimeStep();
   
-  Node *robots = new Node[obstaclesNUmber];
-  Field *translations = new Field[obstaclesNUmber];
-  Field *rotations = new Field[obstaclesNUmber];
+  Motor *leftMotor = supervisor->getMotor("leftMotor");
+  Motor *rightMotor = supervisor->getMotor("rightMotor");  
+ 
+  leftMotor->setPosition(INFINITY);
+  rightMotor->setPosition(INFINITY);
+ 
+  leftMotor->setVelocity(0);
+  rightMotor->setVelocity(0);
   
-  Field *transField = robot_1_0->getField("translation");
-  Field *rotField = robot_1_0->getField("rotation");
+  Client *client = new Client(true, 7777);                      // соединение с SSL-vision/game
   
-  for (int i = 0; i < obstaclesNUmber; i++)
-  {
-    robots[i] = supervisor->getFromDef("robot_2_" + i);
-    translations[i] = robots->getField("translation");
-    rotations[i] = robots->getField("rotation");
+  Node *actor = supervisor->getFromDef("actor");                // поля робота
+  Field *actorTrans = actor->getField("translation");
+  Field *actorRot = actor->getField("rotation");;
+    
+  int n = 7;                                                    // поля препятствий
+  Node *robots[n];
+  Field *trans[n];
+  
+  for (int i = 0; i < n; i ++)  {
+    robots[i] = supervisor->getFromDef("obst" + (i + 1));
+    trans[i] = robots[i]->getField("translation");
   }
   
-  for (int i = 0; i < obstaclesNUmber; i ++)
-  {
-    const double vector[3] = {0, 0, 0}; 
-    translations[i]->setSFVec3f(vector);
-  }
-  
-  while (robot->step(timeStep) != -1) {
+  while (supervisor->step(timeStep) != -1) {
+    protocol::WorldData worldData;
     
+    protocol::Coord actorCoord;
+    const double *translation = actorTrans->getSFVec3f();
+    actorCoord.set_x(translation[0]);
+    actorCoord.set_y(translation[2]);
     
+    protocol::Position actorPos;
+    actorPos.set_allocated_coord(&actorCoord);
+    const double *rot = actorRot->getSFRotation();
+    actorPos.set_theta(rot[3] - 1.57);
     
+    protocol::WheelsVelocity actorVelocity;
+    actorVelocity.set_leftwheelvelocity((int)leftMotor->getVelocity());
+    actorVelocity.set_leftwheelvelocity((int)rightMotor->getVelocity());
+    
+    protocol::RobotData actorData;
+    actorData.set_allocated_position(&actorPos);
+    actorData.set_allocated_wheelsvelocity(&actorVelocity);
+    worldData.set_allocated_robotdata(&actorData);
+        
+    for (int i = 1; i < n; i++){
+      protocol::Coord *obstCoord = worldData.add_obstaclecoords();
+      const double *obstTranslation = trans[i]->getSFVec3f();
+      obstCoord->set_x(obstTranslation[0]);
+      obstCoord->set_y(obstTranslation[2]);
+    }    
+    
+    client->sendWorldData(worldData);
+    protocol::WheelsVelocity newVelocity;
+    client->getNewVelocity(&newVelocity);
+    
+    leftMotor->setVelocity((double)newVelocity.leftwheelvelocity());
+    rightMotor->setVelocity((double)newVelocity.rightwheelvelocity());
   };
 
-  delete robot;
+  delete supervisor;
   return 0;
 }

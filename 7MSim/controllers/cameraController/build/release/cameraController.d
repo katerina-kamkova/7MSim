6439643efef959f9e6295cebb15dab429a904123
katerina <katerina.kamkova@mail.ru>
2020-11-15 19:46:22 +0300

    Clean the project

--- 7MSim/controllers/cameraController/build/release/cameraController.d
+++
@@ -1,6 +0,0 @@
-build/release/cameraController.o: cameraController.cpp \
- /snap/webots/14/usr/share/webots/include/controller/cpp/webots/Robot.hpp \
- /snap/webots/14/usr/share/webots/include/controller/cpp/webots/Camera.hpp \
- /snap/webots/14/usr/share/webots/include/controller/cpp/webots/Device.hpp \
- /snap/webots/14/usr/share/webots/include/controller/cpp/webots/../../c/webots/types.h \
- /snap/webots/14/usr/share/webots/include/controller/cpp/webots/../../c/webots/camera_recognition_object.h
--- 7MSim/controllers/tryController4w/build/release/tryController4w.d
+++
@@ -1,5 +0,0 @@
-build/release/tryController4w.o: tryController4w.cpp \
- /usr/local/webots/include/controller/cpp/webots/Robot.hpp \
- /usr/local/webots/include/controller/cpp/webots/Motor.hpp \
- /usr/local/webots/include/controller/cpp/webots/Device.hpp \
- /usr/local/webots/include/controller/cpp/webots/../../c/webots/types.h

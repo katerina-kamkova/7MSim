92741ee98423d669425030f120d866c1d07927a9
Yaroslav Koekin <ykoekin@gmail.com>
2020-12-16 16:22:43 +0300

    Exclude some robots and set camera in webots project

--- 7MSim/controllers/cameraController/cameraController.cpp
+++ 7MSim/controllers/cameraController/cameraController.cpp
@@ -59,6 +59,8 @@ int main(int argc, char **argv) {
     send(connection, &height, sizeof(unsigned int), 0);
     const unsigned char* picture = camera->getImage();
     send(connection, picture, pictureSize, 0); 
+    
+    sleep(0.5);
   };
   
   close(connection);

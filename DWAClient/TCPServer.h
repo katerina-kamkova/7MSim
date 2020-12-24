b99c3c153573dcc50aed76c6d7efca17f94f6c3f
Yaroslav Koekin <ykoekin@gmail.com>
2020-12-24 12:00:26 +0300

    add gitignore

--- DWAClient/TCPServer.h
+++ DWAClient/TCPServer.h
@@ -1,34 +1,14 @@
 //
-// Created by yarkoe on 11.10.2020.
+// Created by yarkoe on 17.12.2020.
 //
 
 #ifndef DWACLIENT_TCPSERVER_H
 #define DWACLIENT_TCPSERVER_H
 
-#include <string>
-#include <netinet/in.h>
-
-typedef int SOCKET;
-
-enum ClientType {supervisor, robot};
-
-struct Client {
-    ClientType clientType;
-    int clientSocket;
-};
 
 class TCPServer {
-public:
-    TCPServer(unsigned short port);
-    void StartListening();
-    Client AcceptNewClient();
-    static void SendToClient(Client client, char* message, int messageSize);
-    static int ReceiveFromClient(Client client, char* buffer);
-private:
-    static void ExitIfTrue(bool expr, const std::string& exitMessage);
 
-    SOCKET serverSocket;
-    unsigned short serverPort;
 };
+
 
 #endif //DWACLIENT_TCPSERVER_H
